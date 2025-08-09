ae94c60a8be0a665ff92e6f76b01dcd0162a5cb8
Matt Goldberg <matt.q.goldberg@gmail.com>
2025-08-09 14:07:26 -0700

    Moved project organization

--- Makefile
+++
@@ -1,7 +0,0 @@
-TARGET = chess
-
-build:
-	gcc main.c move.c -o $(TARGET)
-
-clean:
-	rm -f $(TARGET)
--- src/data.h
+++ src/data.h
@@ -1,6 +1,11 @@
 #pragma once
 #define BOARD_LENGTH 8
 
+enum Turn {
+	White,
+	Black
+};
+
 typedef struct {
 	int isWhitesTurn;
 	char (*board) [BOARD_LENGTH];
