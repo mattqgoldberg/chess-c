9d93497032e99d48822beee42c19f65e7124b6b0
Matt Goldberg <matt.q.goldberg@gmail.com>
2025-08-22 14:15:20 -0700

    Added pawn tests

--- tests/testMoveset.c
+++ tests/testMoveset.c
@@ -58,15 +58,71 @@ void test_movesetKnight() {
 
 void test_movesetBishop() {
 
+    assert(movesetBishop((Move){{4,4},{5,5}}) == 1);
+    assert(movesetBishop((Move){{4,4},{3,3}}) == 1);
+    assert(movesetBishop((Move){{4,4},{3,5}}) == 1);
+    assert(movesetBishop((Move){{4,4},{5,3}}) == 1);
+
+    assert(movesetBishop((Move){{4,4},{4,5}}) == 0);
+    assert(movesetBishop((Move){{4,4},{5,4}}) == 0);
+    assert(movesetBishop((Move){{4,4},{5,6}}) == 0);
 }
 
 
 void test_movesetQueen() {
 
+    assert(movesetQueen((Move){{4,4},{5,5}}) == 1);
+    assert(movesetQueen((Move){{4,4},{3,3}}) == 1);
+    assert(movesetQueen((Move){{4,4},{3,5}}) == 1);
+    assert(movesetQueen((Move){{4,4},{5,3}}) == 1);
+
+    assert(movesetQueen((Move){{4,4},{4,5}}) == 1);
+    assert(movesetQueen((Move){{4,4},{5,4}}) == 1);
+    assert(movesetQueen((Move){{4,4},{4,0}}) == 1);
+    assert(movesetQueen((Move){{4,4},{0,4}}) == 1);
+    assert(movesetQueen((Move){{4,4},{4,7}}) == 1);
+
+    assert(movesetQueen((Move){{4,4},{5,6}}) == 0);
+    assert(movesetQueen((Move){{4,4},{6,5}}) == 0);
+    assert(movesetQueen((Move){{4,4},{3,2}}) == 0);
+    assert(movesetQueen((Move){{4,4},{2,3}}) == 0);
+    assert(movesetQueen((Move){{4,4},{5,2}}) == 0);
+    assert(movesetQueen((Move){{4,4},{2,3}}) == 0);
+    assert(movesetQueen((Move){{4,4},{6,3}}) == 0);
+    assert(movesetQueen((Move){{4,4},{2,5}}) == 0);
 }
 
 void test_movesetPawn() {
 
+    // White pawn moves
+    assert(movesetPawn((Move){{4,4},{5,4}}, 'P', ' ') == 1);
+    assert(movesetPawn((Move){{4,4},{5,4}}, 'P', 'r') == 0);
+    assert(movesetPawn((Move){{4,4},{6,4}}, 'P', ' ') == 0);
+    assert(movesetPawn((Move){{4,4},{6,4}}, 'P', 'r') == 0);
+    assert(movesetPawn((Move){{4,4},{3,4}}, 'P', ' ') == 0);
+
+    //Move 2 from starting pos
+    assert(movesetPawn((Move){{1,1},{3,1}}, 'P', ' ') == 1);
+
+    //White pawn captures
+    assert(movesetPawn((Move){{1,1},{2,2}}, 'P', 'r') == 1);
+    assert(movesetPawn((Move){{1,1},{2,2}}, 'P', ' ') == 0);
+    assert(movesetPawn((Move){{1,1},{2,0}}, 'P', 'r') == 1);
+    assert(movesetPawn((Move){{1,1},{2,0}}, 'P', ' ') == 0);
+
+    // Black pawn moves
+    assert(movesetPawn((Move){{4,4},{3,4}}, 'p', ' ') == 1);
+    assert(movesetPawn((Move){{4,4},{3,4}}, 'p', 'R') == 0);
+    assert(movesetPawn((Move){{4,4},{2,4}}, 'p', ' ') == 0);
+    assert(movesetPawn((Move){{4,4},{2,4}}, 'p', 'R') == 0);
+
+    //Move 2 from starting pos
+    assert(movesetPawn((Move){{6,1},{4,1}}, 'p', ' ') == 1);
+
+    //White pawn captures
+    assert(movesetPawn((Move){{4,4},{3,3}}, 'p', 'R') == 1);
+    assert(movesetPawn((Move){{4,4},{3,3}}, 'p', ' ') == 0);
+    assert(movesetPawn((Move){{4,4},{3,5}}, 'p', 'R') == 1);
 }
 
 void test_movesetBishopBlocked() {
